45f503c240a3b3db165e895cdf4a4456a603b31e
unknown <you@example.com>
2022-07-13 14:49:17 +1000

    CakeCard component is working now with the initial prices of the cakes

--- src/partials/BlogList.jsx
+++ src/partials/BlogList.jsx
@@ -41,28 +41,6 @@ function BlogList() {
                           <div key={index}>
                             <CakeCard key={index} cake={cake} />
                           </div>
-                          // <div key={cake.id}>
-                          //   <figure className='relative h-0 pb-9/16'>
-                          //     <img 
-                          //       className='absolute inset-0 w-full h-full object-cover' 
-                          //       src={cake.image} 
-                          //       alt="genovese pic" 
-                          //     />
-                          //   </figure>
-                          //   <h3 className='h4 font-red-hat-display mb-2'>{ cake.name}</h3>
-                          //   <p>{ cake.description}</p>
-                          //   <div>{cake.options.map( (cakeOption, index) => {
-                          //     return (
-                          //       <div className='flex flex-wrap' key={index}>
-                          //         <button 
-                          //           className='btn-sm text-white bg-teal-500 hover:bg-teal-400 mb-4'
-                          //           onClick={() => setCakePrice(cakeOption.price)}  
-                          //         >{`${cakeOption.size} ${cakeOption.people}`}</button>
-                          //       </div>
-                          //     )
-                          //   })}</div>
-                          //   <p>${cakePrice}</p>
-                          // </div>
                         )
                       })
                     }
--- src/partials/CakeCard.jsx
+++ src/partials/CakeCard.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const CakeCard = ({cake}) => {
-    let [cakePrice, setCakePrice] = useState(cake.price)
-    
+    let [cakePrice, setCakePrice] = useState(cake.options[0]?.price)
+
+    useEffect(() => {
+        console.log('Hello Uzmi ***', cakePrice);
+    }, [cakePrice]);
+
     return (
         <div className='my-4'>
             <figure className='relative h-0 pb-9/16'>
@@ -13,14 +17,18 @@ const CakeCard = ({cake}) => {
                 />
             </figure>
             <h3 className='h4 font-red-hat-display mb-2'>{ cake.name}</h3>
-            <p>{ cake.description}</p>
-            <div>{cake.options.map((cakeOption, index) => {
+            <p className='text-gray-600 dark:text-gray-400 grow'>{ cake.description}</p>
+            <div className='mt-4'>{cake.options.map((cakeOption, index) => {
                 return (
                     <div className='flex flex-wrap' key={index}>
                         <button 
-                        className='btn-sm text-white bg-teal-500 hover:bg-teal-400 mb-4'
-                        onClick={() => setCakePrice(cakeOption.price)}  
-                        >{`${cakeOption.size} ${cakeOption.people}`}</button>
+                            className='btn-sm text-white bg-teal-500 hover:bg-teal-400 mb-4'
+                            onClick={() => {setCakePrice(cakeOption.price)
+                            }}  
+                        >
+                            {`${cakeOption.size} ${cakeOption.people}`}
+                        </button>
+
                     </div>
                 )
                 })}
