dc5d5a914bd3133f51460381ee1a1b91dffcb82f
unknown <you@example.com>
2022-07-14 08:52:49 +1000

    The CakeCard component was styled with the prices

--- src/partials/CakeCard.jsx
+++ src/partials/CakeCard.jsx
@@ -9,31 +9,35 @@ const CakeCard = ({cake}) => {
 
     return (
         <div className='my-4'>
-            <figure className='relative h-0 pb-9/16'>
+            <figure className='relative h-0 mb-4 pb-9/16'>
                 <img 
                     className='absolute inset-0 w-full h-full object-cover' 
                     src={cake.image} 
                     alt="genovese pic" 
                 />
             </figure>
-            <h3 className='h4 font-red-hat-display mb-2'>{ cake.name}</h3>
-            <p className='text-gray-600 dark:text-gray-400 grow'>{ cake.description}</p>
-            <div className='mt-4'>{cake.options.map((cakeOption, index) => {
-                return (
-                    <div className='flex flex-wrap' key={index}>
-                        <button 
-                            className='btn-sm text-white bg-teal-500 hover:bg-teal-400 mb-4'
-                            onClick={() => {setCakePrice(cakeOption.price)
-                            }}  
-                        >
-                            {`${cakeOption.size} ${cakeOption.people}`}
-                        </button>
+            <div className='flex justify-between'>
+                <div>
+                    <h3 className='h4 font-red-hat-display mb-2'>{ cake.name}</h3>
+                    <p className='text-gray-600 dark:text-gray-400 grow'>{ cake.description}</p>
+                    <div className='mt-4'>{cake.options.map((cakeOption, index) => {
+                        return (
+                            <div key={index}>
+                                <button 
+                                    className='btn-sm text-white bg-teal-500 hover:bg-teal-400 mb-4'
+                                    onClick={() => {setCakePrice(cakeOption.price)
+                                    }}  
+                                >
+                                    {`${cakeOption.size} ${cakeOption.people}`}
+                                </button>
 
+                            </div>
+                        )
+                        })}
                     </div>
-                )
-                })}
+                </div>
+                <h3 className='h4 font-red-hat-display'>${cakePrice}</h3>
             </div>
-            <p>${cakePrice}</p>
         </div>
     );
 }
