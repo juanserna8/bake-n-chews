add8f54b783fc722d14ca298cd9f9b3f788a30af
unknown <you@example.com>
2022-07-22 13:32:29 +1000

    cart icon added in a header button

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -128,7 +128,15 @@ function Header() {
             {/* Desktop CTA on the right */}
             <ul className="flex justify-end flex-wrap items-center">
               <li>
-                <Link to="/contact" className="btn-sm text-white bg-teal-500 hover:bg-teal-400 ml-6">Request code</Link>
+                <Link to="/contact" className="btn-sm text-white bg-teal-500 hover:bg-teal-400 ml-6">
+                  <svg 
+                    className="h-6 w-6 text-white"  
+                    viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  
+                    strokeLinecap="round"  strokeLinejoin="round">  <circle cx="9" cy="21" r="1" />  
+                    <circle cx="20" cy="21" r="1" />  
+                    <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6" />
+                  </svg>
+                </Link>
               </li>
             </ul>
 
