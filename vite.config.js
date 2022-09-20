174cf1f6c3b694335c0d6497fb47c50df451f966
unknown <you@example.com>
2022-09-20 11:46:13 +1000

    vite.config edited

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
         replacement: (val) => {
           return val.replace(/^~/, "");
         },
+        "./runtimeConfig": "./runtimeConfig.browser"
       },
     ],
   },
