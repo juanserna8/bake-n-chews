b97a89adce1eac610e3527a767ee5ec9ef30d474
unknown <you@example.com>
2022-09-23 14:04:01 +1000

    SES is now implemented when someone fills the form in Cta

--- amplify/backend/function/createContact/src/index.js
+++ amplify/backend/function/createContact/src/index.js
@@ -1,17 +1,56 @@
-
+const AWS = require('aws-sdk');
+const SES = new AWS.SES({ region: "us-east-1" });
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-     headers: {
-         "Access-Control-Allow-Origin": "*",
-         "Access-Control-Allow-Headers": "*"
-     }, 
-        body: JSON.stringify('Hello from Lambda!'),
+
+    const parsedBody = JSON.parse(event.body)
+
+    const params = {
+        Destination: {
+            ToAddresses: [parsedBody.email],
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: parsedBody.message
+                }
+            },
+            Subject: {
+                Data: 'Your order'
+            }, 
+        },
+        Source: "sernadominguezj@gmail.com"
     };
+
+    return SES.sendEmail(params).promise()
+    console.log(params)
+    // try {
+    //     const result = await SES.sendEmail(params).promise();
+    //     console.log(result)
+    //     return {
+    //         statusCode: 200,
+    //         body: 'Email sent!'
+    //     }
+    // } catch (e) {
+    //     console.error(e);
+    //     return {
+    //         statusCode: 400,
+    //         body: 'Sending failed'
+    //     }
+    // }
+
+
+    // return {
+    //     statusCode: 200,
+    // //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  }, 
+    //     body: JSON.stringify('Hello from Lambda!'),
+    // };
 };
