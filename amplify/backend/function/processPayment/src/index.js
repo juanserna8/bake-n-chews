d88efbe7ed73cd996e462ebd7bc050ec59f35ef7
unknown <you@example.com>
2022-09-27 15:03:04 +1000

    Email message when customer purchases a cake was modified

--- amplify/backend/function/processPayment/src/index.js
+++ amplify/backend/function/processPayment/src/index.js
@@ -48,7 +48,7 @@ exports.handler = async (event) => {
             Message: {
                 Body: {
                     Text: {
-                        Data: `Your order ${stripeProducts.description} has been processed.`
+                        Data: `Hi, ${<br />}Your order ${stripeProducts.description} has been processed.`
                     }
                 },
                 Subject: {
